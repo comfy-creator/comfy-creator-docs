42164ca2118f8882572a695b246714c5203e8c36
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2024-06-06 16:14:06 +0100

    update baseurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
     url: 'https://comfycreator.com',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/docs/',
+    baseUrl: '/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
@@ -45,7 +45,7 @@ const config: Config = {
             {
 
                 docs: {
-                    routeBasePath: '/',
+                    // routeBasePath: '/',
                     sidebarPath: './sidebars.ts',
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
